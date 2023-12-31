a94b9c53b44d120a9f8e38af4feb5e03f2074fe7
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-12-31 11:56:36 +0100

    Début première méthode CodeGenerator mais aucune idée si ca marche

--- CodeGenerator.java
+++ CodeGenerator.java
@@ -19,39 +19,162 @@ public class CodeGenerator  extends AbstractParseTreeVisitor<Program> implements
 
     @Override
     public Program visitNegation(grammarTCLParser.NegationContext ctx) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitNegation'");
+        Program program = new Program();
+
+        // Récupére le code généré pour l'expression à nier
+        Program exprProgram = visit(ctx.expression());
+
+        // Génére le code assembleur pour la négation
+        int destRegister = getNewRegister();
+        program.addAll(exprProgram);
+        program.addCode("LOADI R" + destRegister + ", 1 ; Load 1 to a register for true");
+        program.addCode("SUB R" + destRegister + ", R" + destRegister + ", R1 ; Subtract expression value from 1");
+        program.addCode("PUSH R" + destRegister + " ; Push the negated value to the stack");
+
+        return program;
     }
+
 
     @Override
     public Program visitComparison(grammarTCLParser.ComparisonContext ctx) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitComparison'");
+        Program program = new Program();
+
+        // Récupére le code généré pour les deux expressions à comparer
+        Program leftExprProgram = visit(ctx.expression(0));
+        Program rightExprProgram = visit(ctx.expression(1));
+
+        // Génére le code assembleur pour la comparaison
+        int destRegister = getNewRegister();
+        program.addAll(leftExprProgram);
+        program.addAll(rightExprProgram);
+
+        // Compare les deux valeurs
+        String operator = ctx.getChild(1).getText();
+        switch (operator) {
+            case "==":
+                program.addCode("POP R2 ; Pop the right operand from the stack");
+                program.addCode("POP R1 ; Pop the left operand from the stack");
+                program.addCode("SUB R" + destRegister + ", R1, R2 ; Subtract left operand from right operand");
+                program.addCode("JZ R" + destRegister + ", TrueLabel ; Jump to TrueLabel if the result is zero");
+                break;
+            case "!=":
+                //...
+                break;
+            case ">":
+                // ...
+                break;
+            default:
+                throw new UnsupportedOperationException("Unsupported comparison operator: " + operator);
+        }
+
+        //  cas False
+        program.addCode("LOADI R" + destRegister + ", 0 ; Load 0 to indicate False");
+        program.addCode("PUSH R" + destRegister + " ; Push the result to the stack");
+        program.addCode("JMP EndLabel ; Jump to EndLabel");
+
+        //  cas True
+        program.addCode("TrueLabel: LOADI R" + destRegister + ", 1 ; Load 1 to indicate True");
+        program.addCode("PUSH R" + destRegister + " ; Push the result to the stack");
+
+        program.addCode("EndLabel: ; End of comparison");
+
+        return program;
     }
+
 
     @Override
     public Program visitOr(grammarTCLParser.OrContext ctx) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitOr'");
+        Program program = new Program();
+
+        // Récupére le code généré pour les deux expressions à comparer
+        Program leftExprProgram = visit(ctx.expression(0));
+        Program rightExprProgram = visit(ctx.expression(1));
+
+        // Génére le code assembleur pour l'opération logique "||"
+        int destRegister = getNewRegister();
+        program.addAll(leftExprProgram);
+
+        // Jump to TrueLabel if left operand is true
+        program.addCode("POP R" + destRegister + " ; Pop the left operand from the stack");
+        program.addCode("JNZ R" + destRegister + ", TrueLabel ; Jump to TrueLabel if the left operand is true");
+
+        // If the left operand is false, evaluate the right operand
+        program.addAll(rightExprProgram);
+        program.addCode("TrueLabel: ; TrueLabel, the left operand is true");
+        program.addCode("POP R" + destRegister + " ; Pop the right operand from the stack");
+
+        // Push the result of the OR operation to the stack
+        program.addCode("OR R" + destRegister + ", R" + destRegister + ", R1 ; Perform OR operation");
+        program.addCode("PUSH R" + destRegister + " ; Push the result to the stack");
+
+        return program;
     }
+
 
     @Override
     public Program visitOpposite(grammarTCLParser.OppositeContext ctx) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitOpposite'");
+        Program program = new Program();
+
+        // Récupére le code généré pour l'expression à nier
+        Program exprProgram = visit(ctx.expression());
+
+        // Génére le code assembleur pour l'opération logique "!"
+        int destRegister = getNewRegister();
+        program.addAll(exprProgram);
+
+        // Jump to TrueLabel if the operand is false
+        program.addCode("POP R" + destRegister + " ; Pop the operand from the stack");
+        program.addCode("JZ R" + destRegister + ", TrueLabel ; Jump to TrueLabel if the operand is false");
+
+        // If the operand is true, push false to the stack
+        program.addCode("FalseLabel: LOADI R" + destRegister + ", 0 ; Load 0 to indicate False");
+        program.addCode("PUSH R" + destRegister + " ; Push the result to the stack");
+        program.addCode("JMP EndLabel ; Jump to EndLabel");
+
+        // If the operand is false, push true to the stack
+        program.addCode("TrueLabel: LOADI R" + destRegister + ", 1 ; Load 1 to indicate True");
+        program.addCode("PUSH R" + destRegister + " ; Push the result to the stack");
+
+        program.addCode("EndLabel: ; End of negation");
+
+        return program;
     }
+
 
     @Override
     public Program visitInteger(grammarTCLParser.IntegerContext ctx) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitInteger'");
+        Program program = new Program();
+
+        // Récupére la valeur entière à partir du contexte
+        int integerValue = Integer.parseInt(ctx.INTEGER().getText());
+
+        // Génére le code assembleur pour charger la constante entière dans un registre
+        int destRegister = getNewRegister();
+        program.addCode("LOADI R" + destRegister + ", " + integerValue + " ; Load integer value to a register");
+        program.addCode("PUSH R" + destRegister + " ; Push the integer value to the stack");
+
+        return program;
     }
+
 
     @Override
     public Program visitTab_access(grammarTCLParser.Tab_accessContext ctx) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitTab_access'");
+        Program program = new Program();
+
+        // Récupérer le type du tableau et l'indice
+        Type tabType = types.get(ctx.ID().getText());
+        Program indexProgram = visit(ctx.expression());
+
+        // Générer le code assembleur pour l'accès au tableau
+        program.addAll(indexProgram);
+        program.addCode("POP R2 ; Pop the index value from the stack");
+        program.addCode("LOAD R1, (R1) ; Load the base address of the array");
+        program.addCode("ADD R1, R1, R2 ; Add the index to the base address");
+        program.addCode("PUSH R1 ; Push the result (address of the array element) to the stack");
+
+        return program;
     }
+
 
     @Override
     public Program visitBrackets(grammarTCLParser.BracketsContext ctx) {
@@ -97,9 +220,32 @@ public class CodeGenerator  extends AbstractParseTreeVisitor<Program> implements
 
     @Override
     public Program visitTab_initialization(grammarTCLParser.Tab_initializationContext ctx) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitTab_initialization'");
+        Program program = new Program();
+
+        // Récupérer le type du tableau
+        Type tabType = types.get(ctx.ID().getText());
+
+        // Générer le code pour l'allocation de mémoire pour le tableau
+        int arraySize = ctx.INT().size();
+        int arrayDestRegister = getNewRegister();
+        program.addCode("LOADI R" + arrayDestRegister + ", " + arraySize + " ; Load array size to a register");
+
+        // logique pour l'allocation de mémoire à faire 
+        // ...
+
+        int currentElementRegister = arrayDestRegister + 1;
+
+        // Générer le code pour chaque élément du tableau
+        for (int i = 0; i < arraySize; i++) {
+            Program exprProgram = visit(ctx.expression(i));
+            program.addAll(exprProgram);
+            program.addCode("STORE R" + currentElementRegister + ", (R" + arrayDestRegister + ") ; Store value in the array");
+            currentElementRegister++;
+        }
+
+        return program;
     }
+
 
     @Override
     public Program visitAddition(grammarTCLParser.AdditionContext ctx) {
