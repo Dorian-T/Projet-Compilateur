af4ec9c1d06d84759750133a184d2a8c4210e5f8
Antonin Slv <antonin.sylvestre@orange.fr>
2024-01-10 16:09:19 +0100

    GROUPE 2 debug dans le main pour

--- Main.java
+++ Main.java
@@ -1,22 +1,31 @@
 import org.antlr.runtime.tree.ParseTree;
+import org.antlr.v4.gui.SystemFontMetrics;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.TokenStream;
 
+import Asm.Instruction;
+import Asm.Program;
+import Asm.UAL;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
         try {
             CharStream input = CharStreams.fromFileName("entrees.txt");
+			System.out.println("input :  \n " + input.toString());
             grammarTCLLexer lexer = new grammarTCLLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-            System.out.println(lexer.getAllTokens());
 
             grammarTCLParser parser = new grammarTCLParser(tokens);
             grammarTCLParser.MainContext tree = parser.main();
             
+			System.out.println("Codegen ; \n");
+			CodeGenerator codeGenerator = new CodeGenerator(null);
+
+			Program program =codeGenerator.visit(tree);
+			System.out.println(program);
 
             //System.out.println(tree);
             //Implémenter une classe NPIStack qui hérite de la classe AbstractParseTree
@@ -25,6 +34,7 @@ public class Main {
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+
     }
 };
 
