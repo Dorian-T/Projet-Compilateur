f21f27f3b589c3061b89c72820a50d90ebf5e42f
antonin <antonin.sylvestre@orange.fr>
2024-01-28 16:32:56 +0100

    G1/2 - main

--- Main.java
+++ Main.java
@@ -1,43 +1,63 @@
-import org.antlr.runtime.tree.ParseTree;
-import org.antlr.v4.gui.SystemFontMetrics;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.TokenStream;
 
-import Asm.Instruction;
 import Asm.Program;
-import Asm.UAL;
+
 
 public class Main {
     public static void main(String[] args) {
-
+        //cahrgement du fichier
+        CharStream input = null;
         try {
-            CharStream input = CharStreams.fromFileName("entrees.txt");
-			System.out.println("input :  \n " + input.toString());
-            grammarTCLLexer lexer = new grammarTCLLexer(input);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            grammarTCLParser parser = new grammarTCLParser(tokens);
-            grammarTCLParser.MainContext tree = parser.main();
-            
-            //-----------------GROUPE 1 : TyperVisitor-----------------
-			TyperVisitor typer = new TyperVisitor();
-            typer.visit(tree);
-            typer.getTypes();
-            //Map<UnknownType, Type> types
-
-            //-----------------GROUPE 2 : CodeGenerator-----------------
-            
-            System.out.println("Codegen ; \n");
-			CodeGenerator codeGenerator = new CodeGenerator(null);
-			Program program =codeGenerator.visit(tree);
-			System.out.println(program);
-
-            
+            input = CharStreams.fromFileName("entrees.txt");
         }catch (Exception e){
+            System.out.println("type d'erreure : " + e.getClass());
             System.out.println(e.getMessage());
         }
 
+        //rappel du fichier + initialisation du lexer et du parser
+        System.out.println("input :  \n " + input.toString());
+        grammarTCLLexer lexer = new grammarTCLLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        grammarTCLParser parser = new grammarTCLParser(tokens);
+        grammarTCLParser.MainContext tree = parser.main();
+        
+        //-----------------GROUPE 1 : TyperVisitor-----------------
+        
+        TyperVisitor typer = new TyperVisitor();
+        typer.visit(tree);
+
+        //-----------------GROUPE 2 : CodeGenerator-----------------
+        
+        CodeGenerator codeGenerator = new CodeGenerator(typer.getTypes());
+        Program program =codeGenerator.visit(tree);
+        toFile(program);
+    // on met dans un fichier
+
+
+    }
+
+    public static void toFile(Program program){
+        try {
+			// Création d'un fileWriter pour écrire dans un fichier
+			FileWriter fileWriter = new FileWriter("prog.asm", false);
+
+			// Création d'un bufferedWriter qui utilise le fileWriter
+			BufferedWriter writer = new BufferedWriter(fileWriter);
+
+			// ajout d'un texte à notre fichier
+			writer.write(program.toString());
+
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 };
 
